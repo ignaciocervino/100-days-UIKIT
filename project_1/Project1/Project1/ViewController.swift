c3e9ede2877ec3cee05b4101d32aa532d22504be
ignaciocervino <ignaciocervino@gmail.com>
2023-01-30 02:35:48 -0300

    finall challenge project1

--- project_1/Project1/Project1/ViewController.swift
+++ project_1/Project1/Project1/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -22,6 +25,7 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        pictures.sort()
 
         print(pictures)
     }
@@ -34,6 +38,14 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.title = "\(indexPath.row + 1) of \(pictures.count)"
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
