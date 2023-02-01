974c8cc4568f98066212e8d53232f691a92f15c9
ignaciocervino <ignaciocervino@gmail.com>
2023-02-01 12:50:03 -0300

    project 4 first part

---
+++ project_4/WebViewApp/WebViewApp/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  WebViewApp
+//
+//  Created by Ignacio Cervino on 01/02/2023.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() { // Gets call before view did load
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+
+        let url = URL(string: "apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true // Let you go forward o backward in the webview with gestures
+    }
+
+    @objc private func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem // Tells iOS when it should attach this in iPad
+        present(ac, animated: true)
+    }
+
+    private func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)! // We know it will be safe
+        webView.load(URLRequest(url: url))
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // When finish loading it page
+        title = webView.title
+    }
+}
+
