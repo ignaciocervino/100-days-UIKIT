fbab74ba2c070c401ca67b59bb8ce6c55762cc7b
ignaciocervino <ignaciocervino@gmail.com>
2023-02-04 20:33:00 -0300

    setup project 5

---
+++ project_5/WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Ignacio Cervino on 04/02/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
