2a6e7d162748a055b69bbb4d0bf741be220dd74e
ignaciocervino <ignaciocervino@gmail.com>
2023-02-05 00:07:21 -0300

    introducing anchors

--- project_6/Project_6_b/Project_6_b/ViewController.swift
+++ project_6/Project_6_b/Project_6_b/ViewController.swift
@@ -48,16 +48,34 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
 
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary)) // H means horizontal layout and pipe the edge of the view, this mean we want the label to go to the edges of the view horizontally
-        }
-
-        let metrics = ["labelHeight": 88]
-
         // USING VFL
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDictionary))
+
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary)) // H means horizontal layout and pipe the edge of the view, this mean we want the label to go to the edges of the view horizontally
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDictionary))
+
+        // Using ANCHORS
+        var previous: UILabel?
+
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+
+            previous = label
+        }
     }
 
 }
