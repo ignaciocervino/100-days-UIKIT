ab5d828aac54cd25de58c9d17bb39a7caa9b3229
ignaciocervino <ignaciocervino@gmail.com>
2023-02-04 23:47:45 -0300

    constraints in code project 6 part one

---
+++ project_6/Project_6_b/Project_6_b/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Project_6_b
+//
+//  Created by Ignacio Cervino on 04/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false // This mean I will make constraints myself
+        label1.backgroundColor = .red
+        label1.text = "THESE 1"
+        label1.sizeToFit()
+
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "THESE 2"
+        label2.sizeToFit()
+
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "THESE 3"
+        label3.sizeToFit()
+
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "THESE 4"
+        label4.sizeToFit()
+
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "THESE 5"
+        label5.sizeToFit()
+
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary)) // H means horizontal layout and pipe the edge of the view, this mean we want the label to go to the edges of the view horizontally
+        }
+
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label4]-[label4]-[label5]", metrics: nil, views: viewsDictionary))
+    }
+
+}
+
