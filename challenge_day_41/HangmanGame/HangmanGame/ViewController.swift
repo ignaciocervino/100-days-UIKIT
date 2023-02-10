6d61810ba7c48e1b6890dfce60862c742b7c7ed9
ignaciocervino <ignaciocervino@gmail.com>
2023-02-10 20:27:21 -0300

    init challenge day 41

---
+++ challenge_day_41/HangmanGame/HangmanGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangmanGame
+//
+//  Created by Ignacio Cervino on 10/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
