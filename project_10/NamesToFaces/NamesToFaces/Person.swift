1c19ec0e406024424acc324826fa7f9fca8b3891
ignaciocervino <ignaciocervino@gmail.com>
2023-02-13 19:38:21 -0300

    fix cells, and add text field action to enter the name for the label

---
+++ project_10/NamesToFaces/NamesToFaces/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Ignacio Cervino on 13/02/2023.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
--- project_10/NamesToFaces/NamesToFaces/ViewController.swift
+++ project_10/NamesToFaces/NamesToFaces/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +33,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             try? jpegData.write(to: imagePath)
         }
 
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+
         dismiss(animated: true) // Dismiss current view controller
     }
 
@@ -41,16 +46,43 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
+
+        let person = people[indexPath.item]
+        cell.nameLabel.text = person.name
+
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+
         return cell
     }
 
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
 
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else { return }
+            person.name = newName
+            self?.collectionView.reloadData()
+
+        })
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac ,animated: true)
+    }
 }
 
