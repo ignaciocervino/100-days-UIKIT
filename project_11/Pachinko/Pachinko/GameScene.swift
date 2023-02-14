02d96e5baf933f2e9f46de7d0b54582ec7fbbbe1
ignaciocervino <ignaciocervino@gmail.com>
2023-02-14 01:01:48 -0300

    add editing mode, to add obstacles

--- project_11/Pachinko/Pachinko/GameScene.swift
+++ project_11/Pachinko/Pachinko/GameScene.swift
@@ -17,6 +17,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
 
+    var editLabel: SKLabelNode!
+
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
+
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -29,6 +41,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.horizontalAlignmentMode = .right
         scoreLabel.position = CGPoint(x: 980, y: 700)
         addChild(scoreLabel)
+
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
 
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
@@ -49,14 +66,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let touch = touches.first else { return }
 
         let location = touch.location(in: self) // where the touch was in the whole screen
+        let objects = nodes(at: location)
 
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        ball.physicsBody?.restitution = 0.4 // Bouncing value
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0 // contactTestBitMask is which collections I want to know about
-        ball.position = location
-        ball.name = "ball"
-        addChild(ball)
+        if objects.contains(editLabel) {
+            editingMode.toggle() // flips boolean
+        } else {
+            if editingMode {
+                // Create a box
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+                addChild(box)
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                ball.physicsBody?.restitution = 0.4 // Bouncing value
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0 // contactTestBitMask is which collections I want to know about
+                ball.position = location
+                ball.name = "ball"
+                addChild(ball)
+            }
+        }
     }
 
     private func makeBouncer(at position: CGPoint) {
