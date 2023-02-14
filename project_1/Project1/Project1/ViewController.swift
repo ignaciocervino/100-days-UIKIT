be665358cb4600fcb56071108b011e0fcf9a46a0
ignaciocervino <ignaciocervino@gmail.com>
2023-02-14 17:57:00 -0300

    Challenge for project 1: Add count for selected images

--- project_1/Project1/Project1/ViewController.swift
+++ project_1/Project1/Project1/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UICollectionViewController {
     var pictures = [String]()
+    var  pictureCount = [String: Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +31,25 @@ class ViewController: UICollectionViewController {
                 DispatchQueue.main.async {
                     self?.collectionView.reloadData()
                 }
+                let defaults = UserDefaults.standard
+                guard let defaultPictureCount = self?.loadPictureCount() else { return }
+
+                self?.pictureCount = defaults.object(forKey: "savedPictures") as? [String: Int] ?? defaultPictureCount
             }
         }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+
+    private func loadPictureCount() -> [String: Int] {
+        var defaultPictureCount = [String: Int]()
+        for item in pictures {
+            defaultPictureCount[item] = 0
+        }
+        return defaultPictureCount
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -42,8 +60,8 @@ class ViewController: UICollectionViewController {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Picture", for: indexPath) as? CustomCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
-
-        cell.nameLabel.text = pictures[indexPath.row]
+        let imageCount = pictureCount[pictures[indexPath.row]]
+        cell.nameLabel.text = "\(pictures[indexPath.row]) - times: \(imageCount ?? 0)"
         cell.imageView.image = UIImage(named: pictures[indexPath.row])
 
         return cell
@@ -60,8 +78,14 @@ class ViewController: UICollectionViewController {
             vc.selectedImage = pictures[indexPath.row]
             vc.title = "\(indexPath.row + 1) of \(pictures.count)"
             navigationController?.pushViewController(vc, animated: true)
+
+            if let currentValue = pictureCount[pictures[indexPath.row]] {
+                pictureCount[pictures[indexPath.row]] = currentValue + 1
+            }
+            savePictureCount()
         }
     }
+
 //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
 //            vc.selectedImage = pictures[indexPath.row]
@@ -69,5 +93,10 @@ class ViewController: UICollectionViewController {
 //            navigationController?.pushViewController(vc, animated: true)
 //        }
 //    }
+
+    private func savePictureCount() {
+        let defaults = UserDefaults.standard
+        defaults.set(pictureCount, forKey: "savedPictures")
+    }
 }
 
