dd937eb3d5aeee9efd005b6278b37b9535f278aa
ignaciocervino <ignaciocervino@gmail.com>
2023-02-15 15:42:55 -0300

    init challenge day 50

---
+++ challenge_day_50/Challenge_day_50/Challenge_day_50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge_day_50
+//
+//  Created by Ignacio Cervino on 15/02/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
