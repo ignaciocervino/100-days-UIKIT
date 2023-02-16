64d22457fe07ef5f58a53484a64bfac75bff578a
ignaciocervino <ignaciocervino@gmail.com>
2023-02-16 01:49:58 -0300

    setup table view

---
+++ challenge_day_50/Challenge_day_50/Challenge_day_50/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  Challenge_day_50
+//
+//  Created by Ignacio Cervino on 15/02/2023.
+//
+
+import Foundation
+
+struct Photo: Codable {
+    var caption: String
+    var image: String
+}
--- challenge_day_50/Challenge_day_50/Challenge_day_50/ViewController.swift
+++ challenge_day_50/Challenge_day_50/Challenge_day_50/ViewController.swift
@@ -7,13 +7,93 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var photos = [Photo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loadUI()
+    }
+
+    private func loadUI() {
+        title = "Photo Listing by PU.Cerviño"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPhoto))
     }
 
 
+    // Objc methods
+    @objc private func addPhoto() {
+        let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    // ImagePicker functions
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appending(path: imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+
+        let photo = Photo(caption: "Unknown", image: imageName)
+        photos.append(photo)
+        // save()
+        tableView.reloadData()
+
+        dismiss(animated: true)
+    }
+
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0] // Returns the documents directory
+    }
+
+    // TableViewController methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        photos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath) as? PhotoCell else {
+            fatalError("Could not dequeue PhotoCell.")
+        }
+        let photo = photos[indexPath.row]
+        let path = getDocumentsDirectory().appendingPathComponent(photo.image)
+
+        cell.photo.image = UIImage(contentsOfFile: path.path)
+        cell.caption.text = photo.caption
+        print("caption: \(photo.caption)")
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath) as? PhotoCell, let image = cell.photo.image else {
+            fatalError("Could not dequeue PhotoCell.")
+        }
+        return tableView.frame.width / image.getCropRatio()
+    }
+
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+}
+
+extension UIImage {
+    func getCropRatio() -> CGFloat {
+        return CGFloat(self.size.width / self.size.height)
+    }
 }
 
