e37047791dc2cfbe1a26b416e2501c8428b464e2
ignaciocervino <ignaciocervino@gmail.com>
2023-02-17 11:36:11 -0300

    init project 14 - WhackAPenguin

---
+++ project_14/WhackAPenguin/WhackAPenguin/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  WhackAPenguin
+//
+//  Created by Ignacio Cervino on 17/02/2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+}
