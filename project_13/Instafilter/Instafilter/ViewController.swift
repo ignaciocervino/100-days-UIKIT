2cca4090dde3f14b6924d66eac2a778a94ee2c2a
ignaciocervino <ignaciocervino@gmail.com>
2023-02-17 00:39:26 -0300

    apply filters

--- project_13/Instafilter/Instafilter/ViewController.swift
+++ project_13/Instafilter/Instafilter/ViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var changeFilterBtn: UIButton!
     var currentImage: UIImage!
+
+    var context: CIContext! // handles rendering
+    var currentFilter: CIFilter! // store filter that user activated
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Instafilter"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     @objc func importPicture() {
@@ -29,15 +38,75 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
         currentImage = image
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 
-    @IBAction func intensityChanged(_ sender: Any) {
+    @IBAction func intensityChanged(_ sender: UIButton) {
+        applyProcessing()
+    }
+
+    @objc func setFilter(_ action: UIAlertAction) {
+        guard currentFilter != nil else { return }
+        guard let actionTitle = action.title else { return }
+
+        currentFilter = CIFilter(name: actionTitle)
+
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+
+        applyProcessing()
     }
 
     @IBAction func save(_ sender: Any) {
     }
 
     @IBAction func changeFilter(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Chose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        if let popoverPresentationController = ac.popoverPresentationController {
+            popoverPresentationController.sourceView = sender
+            popoverPresentationController.sourceRect = sender.bounds
+        }
+
+        present(ac, animated: true)
+    }
+
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+
+        // value of our slider as the intensity of the current filter
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2) , forKey: kCIInputCenterKey)
+        }
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let processImage = UIImage(cgImage: cgImage)
+            imageView.image = processImage
+        }
     }
 }
 
