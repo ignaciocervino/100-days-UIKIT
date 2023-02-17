5e758a679a7ca491b2bd741a74b75adc7ffda21d
ignaciocervino <ignaciocervino@gmail.com>
2023-02-16 23:43:16 -0300

    init project day 52

---
+++ project_13/Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Ignacio Cervino on 16/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+}
+
