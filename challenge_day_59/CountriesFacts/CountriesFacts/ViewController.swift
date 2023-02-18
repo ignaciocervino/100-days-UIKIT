c3bd2dcb67abb6cb8fe6c9f72529147a8f413c47
ignaciocervino <ignaciocervino@gmail.com>
2023-02-18 02:29:59 -0300

    parse data

--- challenge_day_59/CountriesFacts/CountriesFacts/ViewController.swift
+++ challenge_day_59/CountriesFacts/CountriesFacts/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countries = [Country]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getContries()
+        title = "Countries By Ignacio Cerviño"
+    }
+
+    // Consume endpoint
+    func getContries() {
+        guard let url = Bundle.main.url(forResource: "countries", withExtension: "json") else {
+            fatalError("Invalid URL")
+        }
+        DispatchQueue.global(qos: .background).async {
+            if let data = try? Data(contentsOf: url) {
+                self.parse(data)
+            }
+        }
+    }
+
+    private func parse(_ json: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let jsonCountries = try decoder.decode([Country].self, from: json)
+            countries = jsonCountries
+        } catch {
+            fatalError("Error: \(error)")
+        }
     }
 
 
