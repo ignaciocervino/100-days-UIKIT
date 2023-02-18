5860eaac81f6fdd032c29f9b15f3f91da0e510cb
ignaciocervino <ignaciocervino@gmail.com>
2023-02-17 22:32:43 -0300

    init table view UI

---
+++ challenge_day_59/CountriesFacts/CountriesFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesFacts
+//
+//  Created by Ignacio Cervino on 17/02/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
