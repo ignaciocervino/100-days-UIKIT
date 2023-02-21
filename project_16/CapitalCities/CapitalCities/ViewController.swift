d2838e108bef663fb67922099abd45c663b743f9
ignaciocervino <ignaciocervino@gmail.com>
2023-02-21 16:57:49 -0300

    init project 16

---
+++ project_16/CapitalCities/CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Ignacio Cervino on 21/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
