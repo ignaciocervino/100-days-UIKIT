afdb6603ec2d7ceb1fa4265dbd67c9d2fb17cea7
ignaciocervino <ignaciocervino@gmail.com>
2023-02-22 15:42:07 -0300

    using assertion()

--- project_18/Debugging/Debugging/ViewController.swift
+++ project_18/Debugging/Debugging/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         print("I'm inside the viewDidLoad() method.")
         print(1,2,3,4,5, separator: "-")
         print("Some message", terminator: "") // By default the terminator is a linebreak
+
+        assert(1 == 1, "Math failure!") // Condition, message to print if fails
+        assert(1 == 2, "Math failure!")
     }
 
 
