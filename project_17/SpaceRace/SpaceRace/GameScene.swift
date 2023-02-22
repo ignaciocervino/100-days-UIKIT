6b328abe00d7a38e44d8510057019ad6c952468e
Ignacio Cerviño <ignaciocervino@gmail.com>
2023-02-22 03:10:51 -0300

    Project 17: SpaceRace game (#24)

---
+++ project_17/SpaceRace/SpaceRace/GameScene.swift
@@ -0,0 +1,139 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Ignacio Cervino on 22/02/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer?
+    var gameTime = 1.0
+    var enemiesCount = 0
+    var isGameOver = false
+    var touchingPlayer = true
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10) // move 10 seconds of particles
+        addChild(starfield)
+        starfield.zPosition = -1
+
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+
+        score = 0
+
+        physicsWorld.gravity = .zero // we are in the space!
+        physicsWorld.contactDelegate = self // tell us when contact happens
+
+//        gameTimer = Timer.scheduledTimer(timeInterval: gameTime, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+
+    @objc private func createEnemy() {
+        guard let enemy = possibleEnemies.randomElement() else { return }
+
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0 // How fast slow down in time
+        sprite.physicsBody?.angularDamping = 0
+
+        enemiesCount += 1
+        print(enemiesCount)
+
+        if enemiesCount == 20 {
+            gameTime -= 0.1
+            gameTimer?.invalidate()
+            gameTimer = nil
+            enemiesCount = 0
+        }
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+
+        if !isGameOver {
+            score += 1
+        }
+
+        if gameTimer == nil {
+            print("timer \(gameTime)")
+            gameTimer = Timer.scheduledTimer(timeInterval: gameTime, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+
+        var location = touch.location(in: self)
+
+        if location.y < 100 {
+            location.y = 100
+        } else if location.y > 668 {
+            location.y = 668
+        }
+
+        if touchingPlayer {
+            player.position = location
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        let playerXposition =  (player.position.x - player.size.width/2)...(player.position.x + player.size.width/2)
+        let playerYposition =  (player.position.y - player.size.height/2)...(player.position.y + player.size.height/2)
+
+        if !playerXposition.contains(location.x) && !playerYposition.contains(location.y) {
+            touchingPlayer = false
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchingPlayer = true
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = player.position
+        addChild(explosion)
+        player.removeFromParent()
+        isGameOver = true
+        gameTimer?.invalidate()
+    }
+}
