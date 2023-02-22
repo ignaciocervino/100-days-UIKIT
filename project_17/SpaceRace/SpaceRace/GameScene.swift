6de223fec44655c64fb0dcee54f0e81c7ea1d051
ignaciocervino <ignaciocervino@gmail.com>
2023-02-22 01:35:41 -0300

    setup game variables

--- project_17/SpaceRace/SpaceRace/GameScene.swift
+++ project_17/SpaceRace/SpaceRace/GameScene.swift
@@ -8,13 +8,41 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
 
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10) // move 10 seconds of particles
+        addChild(starfield)
+        starfield.zPosition = -1
+
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+
+        score = 0
+
+        physicsWorld.gravity = .zero // we are in the space!
+        physicsWorld.contactDelegate = self // tell us when contact happens
     }
     
     override func update(_ currentTime: TimeInterval) {
