f7f06b75b9e71d19bcef26159d2a3c78f51cded3
ignaciocervino <ignaciocervino@gmail.com>
2023-02-22 01:29:17 -0300

    init project 17: Space race

---
+++ project_17/SpaceRace/SpaceRace/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Ignacio Cervino on 22/02/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
