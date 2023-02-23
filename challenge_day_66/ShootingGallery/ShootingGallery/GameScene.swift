d915a5209699f3ff47867af27fdd17fc01ac0066
ignaciocervino <ignaciocervino@gmail.com>
2023-02-23 02:02:23 -0300

    add static ui

--- challenge_day_66/ShootingGallery/ShootingGallery/GameScene.swift
+++ challenge_day_66/ShootingGallery/ShootingGallery/GameScene.swift
@@ -12,6 +12,31 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
 
+        setUpStaticUI()
+
+
+
+    }
+
+    private func setUpStaticUI() {
+        backgroundColor = .cyan
+
+        let background = SKSpriteNode(imageNamed: "curtains")
+        background.position = CGPoint(x: 512, y: 384)
+        background.size = view!.bounds.size
+        background.zPosition = -1
+        addChild(background)
+
+        var rowYPosition = 590
+        for _ in 0 ..< 3 {
+            let rows = SKSpriteNode(imageNamed: "shelf")
+            rows.position = CGPoint(x: 512, y: rowYPosition)
+            rows.size.width = background.size.width
+            rows.zPosition = -2
+            addChild(rows)
+
+            rowYPosition -= 140
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- challenge_day_66/ShootingGallery/ShootingGallery/GameViewController.swift
+++ challenge_day_66/ShootingGallery/ShootingGallery/GameViewController.swift
@@ -13,5 +13,33 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .fill
+
+                // Present the scene
+                view.presentScene(scene)
+            }
+
+            view.ignoresSiblingOrder = true
+
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 }
