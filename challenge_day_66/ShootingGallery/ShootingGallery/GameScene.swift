9861d549c2090a640af00de6520f3743ebbe3986
ignaciocervino <ignaciocervino@gmail.com>
2023-02-23 01:09:37 -0300

    Init milestone day 66

---
+++ challenge_day_66/ShootingGallery/ShootingGallery/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  ShootingGallery
+//
+//  Created by Ignacio Cervino on 23/02/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ challenge_day_66/ShootingGallery/ShootingGallery/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  ShootingGallery
+//
+//  Created by Ignacio Cervino on 23/02/2023.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
