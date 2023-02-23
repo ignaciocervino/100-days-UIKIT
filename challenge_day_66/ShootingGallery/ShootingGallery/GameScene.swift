49d1c897f71498d9df17307fa5df95d7585e9c91
ignaciocervino <ignaciocervino@gmail.com>
2023-02-23 15:56:47 -0300

    adding cursor and score

--- challenge_day_66/ShootingGallery/ShootingGallery/GameScene.swift
+++ challenge_day_66/ShootingGallery/ShootingGallery/GameScene.swift
@@ -8,7 +8,16 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+    var cursor: SKSpriteNode!
+    var possibleTargets = ["target1", "target2", "target3"]
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
 
@@ -19,6 +28,12 @@ class GameScene: SKScene {
     }
 
     private func setUpStaticUI() {
+        cursor = SKSpriteNode(imageNamed: "cursor")
+        cursor.position = CGPoint(x: 512, y: 384)
+        cursor.physicsBody = SKPhysicsBody(texture: cursor.texture!, size: cursor.size)
+        cursor.physicsBody?.contactTestBitMask = 1
+        addChild(cursor)
+
         backgroundColor = .cyan
 
         let background = SKSpriteNode(imageNamed: "curtains")
@@ -37,9 +52,23 @@ class GameScene: SKScene {
 
             rowYPosition -= 140
         }
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+
+        score = 0
+
+        physicsWorld.gravity = .zero // Don't want gravity
+        physicsWorld.contactDelegate = self // tell us when contact happens
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        cursor.position = location
     }
     
     override func update(_ currentTime: TimeInterval) {
