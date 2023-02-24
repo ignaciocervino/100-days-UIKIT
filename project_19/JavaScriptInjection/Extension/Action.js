4ff51b1b66bc36cfbffff10cefe0c3c161f4d49d
ignaciocervino <ignaciocervino@gmail.com>
2023-02-24 16:19:06 -0300

    adding multiline text with UITextView

--- project_19/JavaScriptInjection/Extension/Action.js
+++ project_19/JavaScriptInjection/Extension/Action.js
@@ -3,11 +3,13 @@ var Action = function() {};
 Action.prototype = {
 
 run: function(parameters) {
-
+    // tell iOS the JavaScript has finished preprocessing, and give this data dictionary to the extension.
+    parameters.completionFunction({"URL": document.URL, "title": document.title });
 },
 
 finalize: function(parameters) {
-
+    var customJavaScript = parameters["customJavaScript"]
+    eval(customJavaScript);
 }
 
 };
