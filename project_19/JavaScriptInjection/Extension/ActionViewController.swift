f8d20ebf4803cc6717623341f4e3f772b0a6f6d8
ignaciocervino <ignaciocervino@gmail.com>
2023-02-24 17:12:01 -0300

    Challenge 1: add alert with pre-written scripts

--- project_19/JavaScriptInjection/Extension/ActionViewController.swift
+++ project_19/JavaScriptInjection/Extension/ActionViewController.swift
@@ -20,6 +20,7 @@ class ActionViewController: UIViewController {
         super.viewDidLoad()
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(addScript))
 
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -52,6 +53,21 @@ class ActionViewController: UIViewController {
         extensionContext?.completeRequest(returningItems: [item])
     }
 
+    @objc func addScript() {
+        let ac = UIAlertController(title: "Choose an script", message: "These are prewritten scripts", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Alert", style: .default) { [weak self] _ in
+            self?.script.text.append(contentsOf: "alert(document.title);\n")
+        })
+        ac.addAction(UIAlertAction(title: "Blur", style: .default) { [weak self] _ in
+            self?.script.text.append(contentsOf: "window.blur();\n")
+        })
+        ac.addAction(UIAlertAction(title: "Open Google", style: .default) { [weak self] _ in
+            self?.script.text.append(contentsOf: "window.open('https://www.google.com');\n")
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
 
@@ -63,7 +79,7 @@ class ActionViewController: UIViewController {
         } else {
             script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
         }
-
+        // Scroll indicator insets control how big the scroll bars are relative to their view.
         script.scrollIndicatorInsets = script.contentInset
 
         let selectedRange = script.selectedRange
