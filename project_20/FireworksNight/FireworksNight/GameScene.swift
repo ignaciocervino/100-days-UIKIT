dcbddca09078346a91ee200d43127550971da0a6
ignaciocervino <ignaciocervino@gmail.com>
2023-02-25 16:43:15 -0300

    setup project 20

---
+++ project_20/FireworksNight/FireworksNight/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  FireworksNight
+//
+//  Created by Ignacio Cervino on 25/02/2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+}
