ef0c14f29cffd3c0b5cf98bed09dbe24fec01175
ignaciocervino <ignaciocervino@gmail.com>
2023-02-27 17:05:44 -0300

    setup CoreLocation and ask authotization request

--- project_22/Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
+++ project_22/Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization() // If you haven't request authorization, it will do it here
+
+        view.backgroundColor = .gray
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+            if CLLocationManager.isRangingAvailable() {
+                // do stuff
+            }
+        }
     }
 
 
