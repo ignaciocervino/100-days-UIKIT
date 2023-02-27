09f59a6cbce7dbee4cc33563d9264e9cb59bd390
ignaciocervino <ignaciocervino@gmail.com>
2023-02-27 18:23:03 -0300

    Init project 23: SwiftyNinja

---
+++ project_23/SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by Ignacio Cervino on 27/02/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+
+    }
+}
