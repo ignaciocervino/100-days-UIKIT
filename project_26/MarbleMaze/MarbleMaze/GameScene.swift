446644a30eca2b92d92d262b333a8b5452fe7ea3
ignaciocervino <ignaciocervino@gmail.com>
2023-03-02 18:00:30 -0300

    init project 26

---
+++ project_26/MarbleMaze/MarbleMaze/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  MarbleMaze
+//
+//  Created by Ignacio Cervino on 02/03/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+
+}
