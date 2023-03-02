3afafe45b409b4a46803c4ba8edb141a27c250b1
ignaciocervino <ignaciocervino@gmail.com>
2023-03-02 10:49:51 -0300

    fix imageView height

--- project_25/SelfieShare/SelfieShare/ViewController.swift
+++ project_25/SelfieShare/SelfieShare/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+
         title = "Selfie Share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
     }
@@ -22,7 +25,8 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageView", for: indexPath)
+        cell.contentView.frame = cell.bounds
 
         if let imageView = cell.viewWithTag(1000) as? UIImageView { // The tag we set in the IB
             imageView.image = images[indexPath.item]
@@ -42,9 +46,16 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
 
-        images.insert(image, at: 0)
+        images.append(image)
         collectionView.reloadData()
     }
 
 }
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 140, height: 140)
+    }
+}
+
 
