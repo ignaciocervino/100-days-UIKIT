49e4771373ac54d12736838ea73546cfea778bb0
ignaciocervino <ignaciocervino@gmail.com>
2023-03-03 23:15:19 -0300

    Prompt the user to import a photo from their photo library.

--- challenge_day_90/MemeGenerator/MemeGenerator/ViewController.swift
+++ challenge_day_90/MemeGenerator/MemeGenerator/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var memeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "Meme Generator"
+        createBarButtons()
+    }
+
+    private func createBarButtons() {
+        let cameraBtn = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(getPicture))
+        navigationItem.rightBarButtonItem = cameraBtn
+    }
+
+    @objc private func getPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+
+        memeImage.image = image
     }
 
 
