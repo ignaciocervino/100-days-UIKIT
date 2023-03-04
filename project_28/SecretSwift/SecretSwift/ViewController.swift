64bef09398c4b52cdf1d35dea6d87a518a36adfe
Ignacio Cerviño <ignaciocervino@gmail.com>
2023-03-04 12:30:55 -0300

    Project 28: Secret Swift (#40)
    * Init project 28: creating a basic text editor, and handle keyboard notifications
    
    * Init project 28: creating a basic text editor, and handle keyboard notifications
    
    * setup iOS keychain
    
    * Touch ID, Face ID and LocalAuthentication
    
    * challenge 1
    
    * challenge 2: password
    
    * add biometric authentication in project 10

---
+++ project_28/SecretSwift/SecretSwift/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  SecretSwift
+//
+//  Created by Ignacio Cervino on 04/03/2023.
+//
+
+import LocalAuthentication
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var secret: UITextView!
+    let password = "unlock"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Nothing to see here"
+
+        let lock = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(lockApp))
+        navigationItem.rightBarButtonItem = lock
+        navigationItem.rightBarButtonItem?.isHidden = true
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+
+    @objc private func lockApp() {
+        saveSecretMessage()
+    }
+
+    @IBAction func authenticateTapped(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+                // so not to block the main thread, UI goes on main
+                DispatchQueue.main.async {
+                    if success {
+                        self?.navigationItem.rightBarButtonItem?.isHidden = false
+                        self?.unlockSecretMessage()
+                    } else {
+                        // error
+                        let ac = UIAlertController(title: "AuthenticationFailed", message: "Your could not be verified, please try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            // no biometry
+//            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default))
+//            present(ac, animated: true)
+
+            let ac = UIAlertController(title: "Enter your password", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] _ in
+                guard let password = ac.textFields?[0].text else { return }
+                if password == "unlock" {
+                    self?.navigationItem.rightBarButtonItem?.isHidden = false
+                    self?.unlockSecretMessage()
+                } else {
+                    let ac = UIAlertController(title: "Wrong password", message: nil, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self?.present(ac, animated: true)
+                }
+            }))
+            present(ac, animated: true)
+        }
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEnd = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEnd, to: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        secret.scrollIndicatorInsets = secret.contentInset
+
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+
+        secret.text = KeychainWrapper.standard.string(forKey: "Secret Message") ?? ""
+    }
+
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
+        navigationItem.rightBarButtonItem?.isHidden = true
+    }
+}
+
