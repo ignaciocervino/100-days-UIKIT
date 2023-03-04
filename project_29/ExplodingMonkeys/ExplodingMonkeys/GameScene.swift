ceb974afd75dc9f7e2948abee72497af02bcca6d
ignaciocervino <ignaciocervino@gmail.com>
2023-03-04 12:54:20 -0300

    Setup project 29

---
+++ project_29/ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ExplodingMonkeys
+//
+//  Created by Ignacio Cervino on 04/03/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+}
