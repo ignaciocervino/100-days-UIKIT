b5a70482983dc782216b666de7fda0dc9dbb327e
ignaciocervino <ignaciocervino@gmail.com>
2023-03-04 03:22:37 -0300

    Add playground with solutions day 91

---
+++ challenge_day_91/Learn-Core-Graphics.playground/Pages/Stripes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+//: # Fixing the fade
+//: This time you'll see the image now contains five rectangles, each 200 pixels wide and the full height of the image. Your designer wanted to make them red, orange, yellow, green, and blue, but they didn't get some of the colors quite right – they look faded.
+//:
+//: - Experiment: Can you draw the five rectangles in their correct colors? The first one has been done for you.
+import UIKit
+
+let rect = CGRect(x: 0, y: 0, width: 1000, height: 1000)
+let renderer = UIGraphicsImageRenderer(bounds: rect)
+
+let rendered = renderer.image { ctx in
+    UIColor.red.setFill()
+    ctx.cgContext.fill(CGRect(x: 0, y: 0, width: 200, height: 1000))
+
+    // Add your code here
+    let colors = [UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue]
+    var colorIndex = 0
+    for i in stride(from: 200, to: 1000, by: 200) {
+        colors[colorIndex].setFill()
+        ctx.cgContext.fill(CGRect(x: i, y: 0, width: 200, height: 1000))
+        colorIndex += 1
+    }
+}
+
+showOutput(rendered)
+//: [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
